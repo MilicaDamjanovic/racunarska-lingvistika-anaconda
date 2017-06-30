4fa132730f63139be8da31cac9b30ab2ce171931
MilicaDamjanovic <dam04.milica@gmail.com>
2017-06-30 17:02:21 +0200

    Add files via upload

---
+++ osnove i sufiksi.py
@@ -0,0 +1,14 @@
+
+rec="pevac"
+rec_osnova=rec[0:3]
+print(rec_osnova)
+rec_sufiks=rec[3:5]
+print(rec_sufiks)
+rec2="beogradski"
+rec2_osnova=rec2[0:7]
+print(rec2_osnova)
+rec2_sufiks=rec2[7:10]
+print(rec2_sufiks)
+rec3="spavanje"
+rec3_osnova=rec3[0:5]
+print(rec3_osnova)
