93fb3044866d8649b6f510b6563ffe030315ecc4
MilicaDamjanovic <dam04.milica@gmail.com>
2017-07-01 17:22:28 +0200

    Add files via upload

---
+++ miki maus.py
@@ -0,0 +1,7 @@
+telefonski_imenik = {"Paja Patak": 123456, "Mini Maus": 234567, "Šilja": 345678}
+vukajlija = {"sojanica": "Posna pljeskavica. Garantovano bez trihinele.", "jahanje": "Omiljena aktivnost šefova za koju je potrebno da radnik ima konjske živce.", "šef": "Čovek koji nema smisao za umor."}
+vrste_reči = {"imenice": ["polaznik", "seminar", "lingvistika", "Isidora"], "glagoli": ["slušati", "crtati", "jesti"], "zamenice": ["on", "ona", "ono"]}
+print(telefonski_imenik.keys())
+print(telefonski_imenik.values())
+vukajlija["šef"]="Miloš"
+print(vukajlija["šef"])
