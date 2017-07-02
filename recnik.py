9b8985eb85cb3f2dc7529c4a28597e86837985a1
MilicaDamjanovic <dam04.milica@gmail.com>
2017-07-02 15:07:25 +0200

    Add files via upload

---
+++ recnik.py
@@ -0,0 +1,2 @@
+recnik={"bitan":{"sinonimi":["vazan","potreban"]},{"antonimi":["nebitan","nevazan"]} "lep":{"sinonimi":["divan","krasan"]},{"antonimi"["ruzan","odvratan"]} "svetao":{"sinonimi":["beo","bled"],"antonimi"["taman","mracan"]}}
+print(recnik["bitan"["sinonimi"]])
